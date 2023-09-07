fbb6cc6ca9ec81f9f471ab84a70119d233197241
monicanovoag <monica.novoa.g@gmail.com>
2023-09-07 16:49:12 -0300

    Actualizacion

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public alertButtons = ['OK'];
+
+  constructor() {}
+
+}
